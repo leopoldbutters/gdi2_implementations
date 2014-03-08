9ab133f5b85b14e7dac18cea6b684cd8bb2abde6
mad <mahdi.cie@gmail.com>
2014-03-08 09:21:40 +0100

    stack javadoc

--- gdi2_implementations/src/de/cie/gdi2/aux/structures/Stack.java
+++ gdi2_implementations/src/de/cie/gdi2/aux/structures/Stack.java
@@ -1,14 +1,26 @@
 package de.cie.gdi2.aux.structures;
 
 public interface Stack<K> {
-	
-	public void push(K k);
-	
-	public K pop();
-	
-	public K top();
-	
-	public boolean isEmpty();
 
+	/**
+	 * @param k
+	 *            push k into stack
+	 */
+	public void push(K k);
+
+	/**
+	 * @return return the head of the stack and remove it
+	 */
+	public K pop();
+
+	/**
+	 * @return return the head of the stack
+	 */
+	public K top();
+
+	/**
+	 * @return returns true if the stack is empty
+	 */
+	public boolean isEmpty();
 
 }
