e7d83a38583c393b146ff69443e0c463a83cae6f
mad <mahdi.cie@gmail.com>
2014-03-08 09:36:34 +0100

    arrayList -TODO implement

---
+++ gdi2_implementations/src/de/cie/gdi2/list/ArrayList.java
@@ -0,0 +1,43 @@
+package de.cie.gdi2.list;
+
+import de.cie.gdi2.aux.structures.LinearSequence;
+
+public class ArrayList<K extends Comparable<K>> extends LinearSequence<K> {
+
+	@Override
+	public int number() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean find(K k) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(K k) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public K findAtPosition(int l) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertAtTail(K k) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertAtPosition(K k, int l) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
